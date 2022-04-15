8ee74884f0e242e24fc3d3afeb51ac71bdbed0aa
andonzhuglov <azhuglov@gmail.com>
2022-04-15 03:23:58 +0300

    hasattr testing

--- ClassPractice/hasattr_test.py
+++ ClassPractice/hasattr_test.py
@@ -1,5 +1,7 @@
 can_we_count_it = [{'s': False}, "sassafrass", 18, ["a", "c", "s", "d", "s"]]
 
 for element in can_we_count_it:
-  if hasattr(can_we_count_it, "count"):
+  if hasattr(element, "count"):
     print(str(type(element)) + " has the count attribute!")
+  else:
+    print(str(type(element)) + " does not have the count attribute :(")
