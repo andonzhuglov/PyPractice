225ac4a1e490a241c38b3ad1a609d3264ace51f4
andonzhuglov <azhuglov@gmail.com>
2022-07-14 12:34:29 +0300

    Completed first CSV Task

---
+++ PandasPractice/CSV/main.py
@@ -0,0 +1,11 @@
+#Extract only the tempratures from a .csv using the import csv
+
+import csv
+
+with open("weather_data.csv") as data_file:
+    data = csv.reader(data_file)
+    temps = []
+    for row in data:
+        if row[1] != "temp":
+            temps.append(int(row[1]))
+    print(temps)
